fd567e8a051934d665c38351d33db1e3867d492c
divineantoinette <divineantoinette.b@gmail.com>
2026-04-29 16:59:31 +0100

    Update SQLQuery1 coffee_DB.sql

--- SQLQuery1 coffee_DB.sql
+++ SQLQuery1 coffee_DB.sql
@@ -1,30 +1,32 @@
 --Import dataset from Excel into a database
 CREATE DATABASE coffee_DB
---Right click, tasks, and import flat file and follow the steps
---Data aggregation using functions SUM(), AVG(), COUNT(), MAX(), MIN()
---Data summarisation using GROUP BY clause
---Data sorting using ORDER BY clause
---Joins in SQL
+  
+--Use the coffee_DB database
 USE coffee_DB;
 SELECT * FROM orders;
 SELECT DISTINCT [product_ID] FROM orders;
+
 --Write a query to summarise quantity ordered by product ID
 SELECT [product_ID], SUM(quantity) AS Total_Quantity
 FROM orders
 GROUP BY [product_ID];
+
 --Write a query to summarise profits by coffee type
 SELECT * FROM Products1;
 SELECT [Coffee_Type], SUM(Profit) AS Total_Profit
 FROM Products1
 GROUP BY [Coffee_Type];
+
 --Write a query to show the number of orders and quantity ordered by product id
 SELECT [product_ID], COUNT(product_ID) AS Number_of_Orders, SUM(quantity) AS Total_Quantity
 FROM orders
 GROUP BY [product_ID];
+
 --Sort them by total quantity in descending order
 ORDER BY Total_Quantity DESC; --NOT WORKING
 
---Joins in SQL
+--Joins in SQL--
+
 --Write a report that shows customer id, customer name, country and total quantity ordered
 SELECT orders.[Customer_ID], customers1.[Customer_Name], customers1.[Country], orders.[quantity]
 FROM orders o
